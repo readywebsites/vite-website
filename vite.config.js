d88c7bbf9b7b1df5bad53a44542c4c0b72e090dd
readywebsites <biz499websites@gmail.com>
2025-05-29 10:53:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/gta6-website/', // 👈 ये GitHub Pages पर सही asset path के लिए ज़रूरी है
   plugins: [react(), tailwindcss()],
 })
